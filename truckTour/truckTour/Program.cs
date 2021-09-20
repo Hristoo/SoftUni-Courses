fe66e7ab7ab86896c1d78bccc7dcedb648c9cc2b
Hristoo <dimitrov_hristo@mail.bg>
2021-09-20 18:00:34 +0300

    trucktour

---
+++ truckTour/truckTour/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace truckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
