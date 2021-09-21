68dcf1845e81e98e8cafda9c9022df2964fb27a3
Hristoo <dimitrov_hristo@mail.bg>
2021-09-21 09:51:20 +0300

    Balanced Parentheses

---
+++ Balanced Parentheses/Balanced Parentheses/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Balanced_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> parantheses = new Stack<char>();
+            bool isBalanced = true;
+
+            foreach (var symbol in input)
+            {
+                if (symbol == '{' || symbol == '[' || symbol == '(')
+                {
+                    parantheses.Push(symbol);
+                }
+                else
+                {
+                    if (parantheses.Count == 0)
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+
+                    if(symbol == '}' && parantheses.Peek() == '{')
+                    {
+                        parantheses.Pop();
+                    }
+                    else if (symbol == ']' && parantheses.Peek() == '[')
+                    {
+                        parantheses.Pop();
+                    }
+                    else if (symbol == ')' && parantheses.Peek() == '(')
+                    {
+                        parantheses.Pop();
+                    }
+                    else
+                    {
+                        isBalanced = false;
+                    }
+                }
+
+            }
+
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
