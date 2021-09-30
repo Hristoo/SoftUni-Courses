559282c66e65e932fb2769119f7773fb4a146e55
Hristoo <dimitrov_hristo@mail.bg>
2021-09-30 16:32:18 +0300

    Basic Stack Operations

---
+++ Basic Stack Operations/Basic Stack Operations/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] stackInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+
+            int smallest = 0;
+
+            bool isFund = false;
+
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                    stack.Push(stackInput[i]);
+            }
+            for (int i = 0; i < s; i++)
+            {
+                stack.TryPop(out stackInput[i]);
+            }
+
+            if (stack.Count > 0)
+            {
+                smallest = stack.Peek();
+            }
+
+            foreach (var item in stack)
+            {
+                if (x == item)
+                {
+                    isFund = true;
+                    
+                    break;
+                }
+                else 
+                {
+                    if (item < smallest)
+                    { 
+                        smallest = item; 
+                    }
+                      
+                }
+            }
+
+            if (isFund)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(smallest);
+            }
+        }
+    }
+}
