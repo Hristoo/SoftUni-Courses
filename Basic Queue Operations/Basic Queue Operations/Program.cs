558f26b5e56af992b9338c423975cdd42addd097
Hristoo <dimitrov_hristo@mail.bg>
2021-09-30 16:31:42 +0300

    queue Basic Queue Operations

---
+++ Basic Queue Operations/Basic Queue Operations/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] stackInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+
+            int smallest = 0;
+
+            bool isFund = false;
+
+
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                queue.Enqueue(stackInput[i]);
+            }
+            for (int i = 0; i < s; i++)
+            {
+                queue.TryDequeue(out stackInput[i]);
+            }
+
+            if (queue.Count > 0)
+            {
+                smallest = queue.Peek();
+            }
+
+            foreach (var item in queue)
+            {
+                if (x == item)
+                {
+                    isFund = true;
+
+                    break;
+                }
+                else
+                {
+                    if (item < smallest)
+                    {
+                        smallest = item;
+                    }
+
+                }
+            }
+
+            if (isFund)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(smallest);
+            }
+        }
+    }
+}
