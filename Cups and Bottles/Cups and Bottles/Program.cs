388558b84be1b6fe8bdb9064276647ed9d7e5b48
Hristoo <dimitrov_hristo@mail.bg>
2021-09-30 16:32:45 +0300

    Cups and Bottles

---
+++ Cups and Bottles/Cups and Bottles/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cups_and_Bottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputCups = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] inputBottles = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> cups = new Queue<int>(inputCups);
+            Stack<int> bottles = new Stack<int>(inputBottles);
+
+            int wastedLiters = 0;
+
+            while (cups.Any() && bottles.Any())
+            {
+                int currentCup = cups.Peek();
+                int currentBottle = bottles.Pop();
+
+                if (currentCup - currentBottle <= 0)
+                {
+                    wastedLiters += currentCup - currentBottle;
+                    cups.Dequeue();
+                }
+                else
+                {
+                    currentCup -= currentBottle;
+                }
+            }
+            if (cups.Any())
+            {
+                Console.WriteLine($"Cuprs: {string.Join(" ", cups)}");
+            }
+            else
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ",bottles)}");
+            }
+
+            Console.WriteLine($"Wasted litters of water: {wastedLiters}");
+        }
+    }
+}
