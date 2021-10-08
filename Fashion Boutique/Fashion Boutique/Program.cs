a895c1a54bf9b8aed0d25f63a8b4875f0e0e559e
Hristoo <dimitrov_hristo@mail.bg>
2021-10-08 16:29:56 +0300

    Fashion Boutique

---
+++ Fashion Boutique/Fashion Boutique/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int inputQ = int.Parse(Console.ReadLine());
+            int counterR = 0;
+            int counterS = 0;
+            int res = 0;
+
+            Stack<int> racks = new Stack<int>(input);
+
+            while (racks.Any())
+            {
+                int current = racks.Peek();
+                counterS += current;
+                if (counterS < inputQ)
+                {
+                    res += current;
+                    racks.Pop();
+                    continue;
+                }
+                else if (counterS == inputQ)
+                {
+                    counterS = 0;
+                    counterR++;
+                    racks.Pop();
+                }
+                else if (counterS > inputQ)
+                {
+                        counterS = 0;
+                        counterS += current;
+                        counterR++;
+                    racks.Pop();
+                }
+                
+            }
+            if (res > 0)
+            {
+                counterR++;
+                Console.WriteLine(counterR);
+            }
+            else
+            {
+                Console.WriteLine(counterR);
+            }
+        }
+    }
+}
