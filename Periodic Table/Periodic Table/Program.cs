2342d6a12603cd407e0570632b1b9aa7baf2b492
Hristoo <dimitrov_hristo@mail.bg>
2021-10-11 11:57:20 +0300

    Periodic Table

---
+++ Periodic Table/Periodic Table/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sets_and_Dictionaries_Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> setUsers = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elements = Console.ReadLine().Split().ToArray();
+
+                foreach (var element in elements)
+                {
+                    setUsers.Add(element);
+                }
+                
+            }
+
+
+                Console.WriteLine(string.Join(" ", setUsers));
+
+
+        }
+    }
+}
