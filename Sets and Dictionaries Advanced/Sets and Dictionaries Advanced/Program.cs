b3d26a6d7e30b46762d7536729cff562a2aa0223
Hristoo <dimitrov_hristo@mail.bg>
2021-10-11 11:46:31 +0300

    Sets and Dictionaries Advanced

---
+++ Sets and Dictionaries Advanced/Sets and Dictionaries Advanced/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sets_and_Dictionaries_Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> setUsers = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+
+                setUsers.Add(Console.ReadLine());
+            }
+
+            foreach (var userName in setUsers)
+            {
+                Console.WriteLine(userName);
+            }
+            
+        }
+    }
+}
