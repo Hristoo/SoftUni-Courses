764bfed71a32d8ae870edde091a9e9171c0ab796
Hristoo <dimitrov_hristo@mail.bg>
2021-10-11 12:14:44 +0300

    Count symbols

---
+++ Count symbols/Count symbols/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sets_and_Dictionaries_Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] charArr = input.ToCharArray();
+
+            SortedDictionary<char, int> chars= new SortedDictionary<char, int>();
+
+
+            foreach (char ch in charArr)
+            {
+                if (!chars.ContainsKey(ch))
+                {
+                    chars.Add(ch, 0);
+                }
+                chars[ch]++;
+            }
+            
+            foreach (var ch in chars)
+
+                Console.WriteLine($"{ch.Key}: {ch.Value} time/s");
+        }
+    }
+}
