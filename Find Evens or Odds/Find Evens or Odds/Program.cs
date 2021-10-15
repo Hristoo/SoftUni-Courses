9df93663f3a6d0e2c5927b46449b8e9fabb504dd
Hristo Dimitrov <dimitrov_hristo@mail.bg>
2021-10-15 12:34:22 +0300

    Add files via upload

---
+++ Find Evens or Odds/Find Evens or Odds/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Evens_or_Odds
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Predicate<int> isEven = number => number % 2 == 0;
+			Action<int[]> printNumbers = inpupNumbers
+				=> Console.WriteLine(string.Join(" ", inpupNumbers)); ;
+
+			List<int> numbers = new List<int>();
+
+			int[] range = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+
+			for (int i = range[0]; i <= range[1]; i++)
+			{
+				numbers.Add(i);
+			}
+
+			string evenOdd = Console.ReadLine();
+
+			if (evenOdd == "even")
+			{
+				printNumbers(numbers.Where(x => isEven(x)).ToArray());
+			}
+			else
+			{
+				printNumbers(numbers.Where(x => !isEven(x)).ToArray());
+			}
+		}
+	}
+}
