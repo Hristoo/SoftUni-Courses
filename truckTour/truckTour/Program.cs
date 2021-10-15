aab9ae6259cb068a5e4c72d3973ea61fef42491c
Hristoo <dimitrov_hristo@mail.bg>
2021-10-15 12:31:10 +0300

    pull

--- truckTour/truckTour/Program.cs
+++ truckTour/truckTour/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace truckTour
 {
@@ -6,7 +8,49 @@ namespace truckTour
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<int[]> truckTour = new Queue<int[]>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine().Split()
+                    .Select(int.Parse).ToArray();
+
+                int petrol = input[0];
+                int distance = input[1];
+
+                truckTour.Enqueue(new int[] { petrol, distance });
+            }
+            int startIndex = 0;
+
+            while (true)
+            {
+                int currenrPetrol = 0;
+
+                foreach (var info in truckTour)
+                {
+                    int truckPetrol = info[0];
+                    int truckDistance = info[1];
+
+                    currenrPetrol += truckPetrol;
+                    currenrPetrol -= truckDistance;
+
+                    if (currenrPetrol < 0)
+                    {
+                        int[] element = truckTour.Dequeue();
+                        truckTour.Enqueue(element);
+                        startIndex++;
+                        break;
+                    }
+                }
+
+                if (currenrPetrol >= 0)
+                {
+                    Console.WriteLine(startIndex);
+                    break;
+                }
+            }
         }
     }
 }
