e81e9b2a4f233548de36ec4610ac999f3856f66b
Hristoo <dimitrov_hristo@mail.bg>
2021-10-15 12:27:55 +0300

    Predicate for Names

---
+++ Predicate for Names/Predicate for Names/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Predicate_for_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int, bool> nameLenght =
+                (name, lenght) => name.Length <= lenght;
+
+            int maxLenghtName = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split()
+                .Where(x => nameLenght(x, maxLenghtName))
+                .ToArray();
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
